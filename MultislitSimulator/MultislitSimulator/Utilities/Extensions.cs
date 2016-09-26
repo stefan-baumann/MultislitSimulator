ac3944e76e687f00cf57572f22a29768ee2696b2
Stefan Baumann <stefan-baumann@outlook.com>
2016-09-26 17:37:01 +0200

    Added extension methods

---
+++ MultislitSimulator/MultislitSimulator/Utilities/Extensions.cs
@@ -0,0 +1,41 @@
+﻿/* Copyright (c) 2016 Stefan Baumann
+ * This code is distributed under the terms of the MIT License (https://opensource.org/licenses/MIT)
+ * GitHub Repository: https://github.com/stefan-baumann/MultislitSimulator
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultislitSimulator.Utilities
+{
+    /// <summary>
+    /// Provides simple extension methods for existing types.
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Clamps this value to the specified range.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="val">The value.</param>
+        /// <param name="min">The minimum value.</param>
+        /// <param name="max">The maximum value.</param>
+        /// <returns></returns>
+        public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T>
+        {
+            if (val.CompareTo(min) < 0)
+            {
+                return min;
+            }
+            if (val.CompareTo(max) > 0)
+            {
+                return max;
+            }
+
+            return val;
+        }
+    }
+}
