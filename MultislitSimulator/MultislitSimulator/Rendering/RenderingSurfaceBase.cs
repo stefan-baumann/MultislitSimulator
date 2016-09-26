e89b59b8fe47f0e964961156d053eff4ab06f824
Stefan Baumann <stefan-baumann@outlook.com>
2016-09-26 17:38:18 +0200

    Added the rendering surface base.

---
+++ MultislitSimulator/MultislitSimulator/Rendering/RenderingSurfaceBase.cs
@@ -0,0 +1,44 @@
+﻿/* Copyright (c) 2016 Stefan Baumann
+ * This code is distributed under the terms of the MIT License (https://opensource.org/licenses/MIT)
+ * GitHub Repository: https://github.com/stefan-baumann/MultislitSimulator
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultislitSimulator.Rendering
+{
+    public abstract class RenderingSurfaceBase
+        : Control
+    {
+        protected RenderingSurfaceBase()
+        {
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.ResizeRedraw | ControlStyles.UserPaint, true);
+        }
+
+        protected abstract void Render(Graphics g);
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            using (BufferedGraphics g = BufferedGraphicsManager.Current.Allocate(e.Graphics, new Rectangle(Point.Empty, this.Size)))
+            {
+                //g.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+                this.Render(g.Graphics);
+
+                g.Graphics.DrawString("Stefan Baumann", this.Font, Brushes.White, new Rectangle(Point.Empty, new Size(this.Width - 10, this.Height - 5)), new StringFormat() { Alignment = StringAlignment.Far, LineAlignment = StringAlignment.Far });
+
+                g.Render();
+            }
+
+            base.OnPaint(e);
+        }
+    }
+}
